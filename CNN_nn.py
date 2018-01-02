a2598f4571d7e9775df64adaa384979970a6de18
sbpark <kaem9932@gmail.com>
2018-01-02 19:46:01 +0900

    CNN 파라미터 저장 - 레이어 구성완료 --- to do --- 1. 모듈화 2. train(학습 서버)과 test(운용 서버)나눔

--- CNN_nn.py
+++ CNN_nn.py
@@ -17,22 +17,115 @@ INPUT_SIZE = 1024
 HIDDEN_SIZE = 500
 OUTPUT_SIZE = 2
 
-learning_rate = 0.01
-training_epoch = 1000
-batch_size = 200
+learning_rate = 0.0001
+training_epoch = 100
+batch_size = 100
 epoch_step = 10
+
+# Get data
+pre._reader(p_path, p_total_list)
+pre._reader(e_path, p_total_list)
+print(p_total_list.__len__())
+pre._reader(np_path, np_total_list)
+pre._reader(o_path, np_total_list)
+print(np_total_list.__len__())
+
+# resize
+p_total_list = pre._resize(p_total_list)
+np_total_list = pre._resize(np_total_list)
+
+# divide
+X_training, Y_training, X_validation, Y_validation, X_test, Y_test = pre._shuffleNdivide(p_total_list, np_total_list)
+print(X_training.shape,", ",X_validation.shape,", ",X_test.shape,", ",Y_training.shape,", ",Y_validation.shape,", ",Y_test.shape)
+X_training = X_training.reshape(-1, INPUT_SIZE, 1)
+# Y_training = Y_training.reshape(-1, 2, 1)
+X_validation = X_validation.reshape(-1, INPUT_SIZE, 1)
+# Y_validation = Y_validation.reshape(-1, 2, 1)
+X_test = X_test.reshape(-1, INPUT_SIZE, 1)
+# Y_test = Y_test.reshape(-1, 2, 1)
+print(X_training.shape,", ",X_validation.shape,", ",X_test.shape)
+
+print('Data constructed!')
 
 
 X = tf.placeholder(tf.float32, [None, INPUT_SIZE, 1])
 Y = tf.placeholder(tf.float32, [None, 2])
 
-W1 = tf.Variable(tf.random_normal([16, batch_size, 8], stddev=0.01))
+# width( size of feature map), input channel, output channel
+W1 = tf.Variable(tf.random_normal([8, 1, 3], stddev=0.01))
 print(W1.shape)
 C1 = tf.nn.conv1d(value=X, filters=W1, stride=1, padding='SAME', name='conv1')
-C1 = tf.nn.relu(C1)
+C1 = tf.nn.tanh(C1)
 
-W2 = tf.Variable(tf.random_normal([batch_size, 16, 8]))
+W2 = tf.Variable(tf.random_normal([16, 3, 5]))
 C2 = tf.nn.conv1d(value=C1, filters=W2, stride=1, padding='SAME', name='conv2')
+C2 = tf.nn.tanh(C2)
+
+W3 = tf.Variable(tf.random_normal([8, 5, 3]))
+C3 = tf.nn.conv1d(value=C2, filters=W3, stride=1, padding='SAME', name='conv3')
+C3 = tf.nn.tanh(C3)
+
+P1 = tf.layers.max_pooling1d(inputs=C3, pool_size=2, padding='SAME', strides=2)
+
+flat = tf.reshape(P1, [-1, 512*3])
+
+W4 = tf.Variable(tf.random_normal([512*3, 200]))
+b4 = tf.Variable(tf.random_normal([200]))
+dense4 = tf.nn.softsign(tf.matmul(flat, W4) + b4)
+
+W5 = tf.Variable(tf.random_normal([200, 2]))
+b5 = tf.Variable(tf.random_normal([2]))
+dense5 = tf.nn.softsign(tf.matmul(dense4, W5) + b5)
+
+p = tf.nn.softmax(dense5)
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=dense5, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# define validation function
+h_predict = tf.argmax(p, 1)
+correct_y = tf.argmax(Y, 1)
+correct_prediction = tf.equal(tf.argmax(p, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
 
 print(C1)
 print(C2)
+print(C3)
+print(P1)
+print(flat)
+print(dense4)
+print(dense5)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for epoch in range(training_epoch):
+    #Batch processing
+    avg_loss = 0.0
+    acc = 0.0
+    total_batch = int(X_training.__len__() / batch_size)
+    for i in range(total_batch):
+        batch_xs = X_training[i*batch_size:(i+1)*batch_size]
+        batch_ys = Y_training[i*batch_size:(i+1)*batch_size]
+        """ Training """
+        # feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: [0.1, 0.2, 0.3]}
+        feed_dict = {X: batch_xs, Y: batch_ys}
+        _, c = sess.run([optimizer, cost], feed_dict=feed_dict)
+        avg_loss += c / total_batch
+        # print('Batch: ', (i+1), ', cost: ', c)
+        # print('----------------------------------')
+    if epoch % epoch_step == 0:
+        print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_loss))
+
+    """ Validation """
+    if (epoch % 5 == 0) :
+        loss_val, acc_val, v_Y, v_p = sess.run([cost, accuracy, correct_y, h_predict], feed_dict={X: X_validation, Y: Y_validation})
+        # print(v_Y[:30])
+        # print(v_p[:30])
+        print('-- validation -- Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, loss_val, acc_val))
+        # print('-- validation -- Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, loss_val, acc_val))
+
+        # if epoch %10 == 0 : print('Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, avg_loss, acc))
+
+print("learning finished!")
