311a4583d70d89e1878fdb5ed8eebd1b4b33b800
sbpark <kaem9932@gmail.com>
2018-01-09 20:35:56 +0900

    CNN 파라미터 저장완료

--- CNN_nn.py
+++ CNN_nn.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-import csv
-import numpy as np
 import Preprocess as pre
 from sklearn.metrics import precision_score, recall_score, f1_score
 
@@ -48,84 +46,95 @@ print(X_training.shape,", ",X_validation.shape,", ",X_test.shape)
 print('Data constructed!')
 
 
-X = tf.placeholder(tf.float32, [None, INPUT_SIZE, 1])
-Y = tf.placeholder(tf.float32, [None, 2])
 
-# width( size of feature map), input channel, output channel
-W1 = tf.Variable(tf.random_normal([8, 1, 3], stddev=0.01))
-print(W1.shape)
-C1 = tf.nn.conv1d(value=X, filters=W1, stride=1, padding='SAME', name='conv1')
-C1 = tf.nn.tanh(C1)
+def _model(X):
 
-W2 = tf.Variable(tf.random_normal([16, 3, 5]))
-C2 = tf.nn.conv1d(value=C1, filters=W2, stride=1, padding='SAME', name='conv2')
-C2 = tf.nn.tanh(C2)
+    # width( size of feature map), input channel, output channel
+    W1 = tf.Variable(tf.random_normal([8, 1, 3], stddev=0.01))
+    C1 = tf.nn.conv1d(value=X, filters=W1, stride=1, padding='SAME', name='conv1')
+    C1 = tf.nn.tanh(C1)
 
-W3 = tf.Variable(tf.random_normal([8, 5, 3]))
-C3 = tf.nn.conv1d(value=C2, filters=W3, stride=1, padding='SAME', name='conv3')
-C3 = tf.nn.tanh(C3)
+    W2 = tf.Variable(tf.random_normal([16, 3, 5]))
+    C2 = tf.nn.conv1d(value=C1, filters=W2, stride=1, padding='SAME', name='conv2')
+    C2 = tf.nn.tanh(C2)
 
-P1 = tf.layers.max_pooling1d(inputs=C3, pool_size=2, padding='SAME', strides=2)
+    W3 = tf.Variable(tf.random_normal([8, 5, 3]))
+    C3 = tf.nn.conv1d(value=C2, filters=W3, stride=1, padding='SAME', name='conv3')
+    C3 = tf.nn.tanh(C3)
 
-flat = tf.reshape(P1, [-1, 512*3])
+    P1 = tf.layers.max_pooling1d(inputs=C3, pool_size=2, padding='SAME', strides=2)
 
-W4 = tf.Variable(tf.random_normal([512*3, 200]))
-b4 = tf.Variable(tf.random_normal([200]))
-dense4 = tf.nn.softsign(tf.matmul(flat, W4) + b4)
+    flat = tf.reshape(P1, [-1, 512*3])
 
-W5 = tf.Variable(tf.random_normal([200, 2]))
-b5 = tf.Variable(tf.random_normal([2]))
-dense5 = tf.nn.softsign(tf.matmul(dense4, W5) + b5)
+    W4 = tf.Variable(tf.random_normal([512*3, 200]))
+    b4 = tf.Variable(tf.random_normal([200]))
+    dense4 = tf.nn.softsign(tf.matmul(flat, W4) + b4)
 
-p = tf.nn.softmax(dense5)
+    W5 = tf.Variable(tf.random_normal([200, 2]))
+    b5 = tf.Variable(tf.random_normal([2]))
+    dense5 = tf.nn.softsign(tf.matmul(dense4, W5) + b5)
 
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=dense5, labels=Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+    param_list = [W1, W2, W3, W4, W5, b4, b5]
 
-# define validation function
-h_predict = tf.argmax(p, 1)
-correct_y = tf.argmax(Y, 1)
-correct_prediction = tf.equal(tf.argmax(p, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    saver = tf.train.Saver(param_list)
 
+    return dense5, saver
 
-print(C1)
-print(C2)
-print(C3)
-print(P1)
-print(flat)
-print(dense4)
-print(dense5)
+def _train(hypothesis, label):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=label))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+    return optimizer, cost
 
-for epoch in range(training_epoch):
-    #Batch processing
-    avg_loss = 0.0
-    acc = 0.0
-    total_batch = int(X_training.__len__() / batch_size)
-    for i in range(total_batch):
-        batch_xs = X_training[i*batch_size:(i+1)*batch_size]
-        batch_ys = Y_training[i*batch_size:(i+1)*batch_size]
-        """ Training """
-        # feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: [0.1, 0.2, 0.3]}
-        feed_dict = {X: batch_xs, Y: batch_ys}
-        _, c = sess.run([optimizer, cost], feed_dict=feed_dict)
-        avg_loss += c / total_batch
-        # print('Batch: ', (i+1), ', cost: ', c)
-        # print('----------------------------------')
-    if epoch % epoch_step == 0:
-        print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_loss))
+def _test(hypothesis, label):
 
-    """ Validation """
-    if (epoch % 5 == 0) :
-        loss_val, acc_val, v_Y, v_p = sess.run([cost, accuracy, correct_y, h_predict], feed_dict={X: X_validation, Y: Y_validation})
-        # print(v_Y[:30])
-        # print(v_p[:30])
-        print('-- validation -- Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, loss_val, acc_val))
-        # print('-- validation -- Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, loss_val, acc_val))
+    # hypothesis = dense5
+    p = tf.nn.softmax(hypothesis)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=label))
 
-        # if epoch %10 == 0 : print('Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, avg_loss, acc))
+    # define validation function
+    h_predict = tf.argmax(p, 1)
+    correct_y = tf.argmax(label, 1)
+    correct_prediction = tf.equal(tf.argmax(p, 1), tf.argmax(label, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-print("learning finished!")
+    return cost, accuracy, correct_y, h_predict
+
+def _main():
+    X = tf.placeholder(tf.float32, [None, INPUT_SIZE, 1])
+    Y = tf.placeholder(tf.float32, [None, OUTPUT_SIZE])
+
+    hypo, model_saver = _model(X=X)
+    optimizer, cost = _train(hypothesis=hypo, label=Y)
+    cost, accuracy, correct_y, h_predict = _test(hypothesis=hypo, label=Y)
+
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(training_epoch):
+        #Batch processing
+        avg_loss = 0.0
+        acc = 0.0
+        total_batch = int(X_training.__len__() / batch_size)
+        for i in range(total_batch):
+            batch_xs = X_training[i*batch_size:(i+1)*batch_size]
+            batch_ys = Y_training[i*batch_size:(i+1)*batch_size]
+            """ Training """
+            feed_dict = {X: batch_xs, Y: batch_ys}
+            _, c = sess.run([optimizer, cost], feed_dict=feed_dict)
+            avg_loss += c / total_batch
+        if epoch % epoch_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost={:.9f}".format(avg_loss))
+
+        """ Validation """
+        if (epoch % 5 == 0) :
+            loss_val, acc_val, v_Y, v_p = sess.run([cost, accuracy, correct_y, h_predict], feed_dict={X: X_validation, Y: Y_validation})
+            print('-- validation -- Epoch: %d, Loss: %f, Accuracy: %f'%(epoch, loss_val, acc_val))
+
+    save_path = model_saver.save(sess, './saved_model_cnn/cnn_param.ckpt')
+    print('save path', save_path)
+
+    sess.close()
+    print("learning finished!")
+
+_main()
